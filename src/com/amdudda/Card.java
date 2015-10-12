ea46942dba609f0c851cd6f424c6c888da04be94
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-12 15:11:58 -0500

    Initial commit

---
+++ src/com/amdudda/Card.java
@@ -0,0 +1,51 @@
+package com.amdudda;
+
+/**
+ * Created by amdudda on 10/12/2015.
+ */
+public class Card {
+    // card object for Crazy Eights Game
+
+    protected String suit;
+    protected int value;
+
+    // constructor
+    public Card(String s, int v) {
+        this.suit = s;
+        this.value = v;
+    }
+
+    // setters and getters
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    // end setters and getters
+
+    // misc methods
+    @Override
+    public String toString() {
+        return this.getFace() + " of " + this.suit;
+    }
+
+    private String getFace() {
+        switch (this.value) {
+            case 1: return "Ace";
+            case 11: return "Jack";
+            case 12: return "Queen";
+            case 13: return "King";
+            default: return this.value + "";
+        }
+    }
+}
---
+++ src/com/amdudda/CrazyEightsGame.java
@@ -0,0 +1,9 @@
+package com.amdudda;
+
+public class CrazyEightsGame {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
